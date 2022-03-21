e3c43cc6ae78dffe46ab6db6169bc9af595a08e8
VadimChorrny <chorrny228@gmail.com>
2022-03-21 12:59:17 +0200

    Add project files.

---
+++ Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+        public static class ServiceExtensions
+        {
+            public static void AddDbContext(this IServiceCollection services,
+                string connectionString)
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            }
+            public static void AddRepository(this IServiceCollection services)
+            {
+                services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            }
+            public static void AddUnitOfWork(this IServiceCollection services)
+            {
+                services.AddTransient<IUnitOfWork, UnitOfWork>();
+            }
+        }
+}
